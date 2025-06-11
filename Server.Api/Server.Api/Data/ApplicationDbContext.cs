ded9b00e227fcebc25fd7a35a7904254de3d7d65
Kirito54 <33008107+Kirito54@users.noreply.github.com>
2025-06-11 22:44:58 +0300

    Implement SED integration service

--- Server.Api/Server.Api/Data/ApplicationDbContext.cs
+++ Server.Api/Server.Api/Data/ApplicationDbContext.cs
@@ -11,4 +11,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     }
 
     public DbSet<Department> Departments { get; set; } = default!;
+    public DbSet<SedDocumentLog> SedDocumentLogs { get; set; } = default!;
 }
