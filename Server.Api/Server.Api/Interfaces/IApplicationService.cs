772a705256d149195bddcf3cbae49808d8f70c33
Kirito54 <33008107+Kirito54@users.noreply.github.com>
2025-06-11 22:39:42 +0300

    Implement ApplicationService

--- Server.Api/Server.Api/Interfaces/IApplicationService.cs
+++ Server.Api/Server.Api/Interfaces/IApplicationService.cs
@@ -1,3 +1,14 @@
+using GovServices.Server.DTOs;
+
 namespace GovServices.Server.Interfaces;
 
-public interface IApplicationService { }
+public interface IApplicationService
+{
+    Task<List<ApplicationDto>> GetAllAsync();
+    Task<ApplicationDto> GetByIdAsync(int id);
+    Task<ApplicationDto> CreateAsync(CreateApplicationDto dto);
+    Task UpdateAsync(int id, UpdateApplicationDto dto);
+    Task DeleteAsync(int id);
+    Task AdvanceAsync(int applicationId, object contextData);
+    Task<List<ApplicationLogDto>> GetLogsAsync(int applicationId);
+}
