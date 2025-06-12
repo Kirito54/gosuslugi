f61ee5ffa92523b43067ef074d998edb025f5482
Kirito54 <33008107+Kirito54@users.noreply.github.com>
2025-06-12 12:01:45 +0300

    Implement TemplateService with PDF generation

---
+++ Server.Api/Server.Api/DTOs/UpdateTemplateDto.cs
@@ -0,0 +1,9 @@
+namespace GovServices.Server.DTOs
+{
+    public class UpdateTemplateDto
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+    }
+}
