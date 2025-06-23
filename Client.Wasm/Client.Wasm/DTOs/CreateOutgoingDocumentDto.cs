d696a0a89cf67a44ca4f418d0626283b3fafac09
Kirito54 <33008107+Kirito54@users.noreply.github.com>
2025-06-23 12:42:40 +0300

    Remove outdated AspNetCore packages and adjust DTOs

--- Client.Wasm/Client.Wasm/DTOs/CreateOutgoingDocumentDto.cs
+++ Client.Wasm/Client.Wasm/DTOs/CreateOutgoingDocumentDto.cs
@@ -1,10 +1,10 @@
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Components.Forms;
 namespace Client.Wasm.DTOs
 {
     public class CreateOutgoingDocumentDto
     {
         public int ApplicationId { get; set; }
-        public IFormFile File { get; set; }
-        public List<IFormFile> Attachments { get; set; }
+        public IBrowserFile File { get; set; }
+        public List<IBrowserFile> Attachments { get; set; }
     }
 }
